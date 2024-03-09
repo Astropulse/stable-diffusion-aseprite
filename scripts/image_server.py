Fix NVIDIA 9xx and server gpu precision

scripts/image_server.py
@@ -184,6 +184,8 @@ def play(file):
 
 # Calculate precision mode by gpu
 def get_precision(device, precision):
+    model_precision = torch.float32
+    vae_precision = torch.float32
     if device == "cuda" and torch.cuda.is_available() and not precision == "fp32":
 
         gpu_name = torch.cuda.get_device_name(device)
@@ -225,8 +227,44 @@ def get_precision(device, precision):
         # If GPU is not nvidia
         elif not "NVIDIA" in gpu_name:
             precision = "fp32"
-            vae_precision = torch.float32
             model_precision = torch.float32
+            vae_precision = torch.float32
+
+        # Device is not recognized, run manual checks
+        else:
+            if precision == "fp32":
+                model_precision = torch.float32
+                vae_precision = torch.float32
+            elif precision =="fp16" or precision == "fp8":
+                # Check for FP16 support
+                try:
+                    _ = torch.ones(1, dtype=torch.float16).cuda()
+                    model_precision = torch.float16
+                    vae_precision = torch.float16
+                    # Check for BF16 support
+                    try:
+                        _ = torch.ones(1, dtype=torch.bfloat16).cuda()
+                        model_precision = torch.bfloat16
+                        vae_precision = torch.bfloat16
+                    except:
+                        pass
+                    # Check for fp8 support
+                    if precision == "fp8":
+                        try:
+                            _ = _.to(torch.float8_e4m3fn).cuda()
+                            model_precision = torch.float8_e4m3fn
+                            vae_precision = torch.bfloat16
+                        except:
+                            # fp8 is not supported, fallback to fp16
+                            precision = "fp16"
+                            model_precision = torch.float16
+                            vae_precision = torch.float16
+                except:
+                    # fp16 is not supported, fallback to fp32
+                    precision = "fp32"
+                    model_precision = torch.float32
+                    vae_precision = torch.float32
+                    
 
     else:
         # Fallback to fp32 precision