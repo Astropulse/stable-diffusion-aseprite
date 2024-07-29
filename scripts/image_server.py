5b855b80c33b0444655ded7dad799a20ce17dd7e
Astropulse <Astropulse@fastmail.com>
2024-07-29 10:47:06 -0400

    Version bump

--- scripts/image_server.py
+++ scripts/image_server.py
@@ -149,7 +149,7 @@ system_models = ["quality", "adapter", "crop", "detail", "brightness", "contrast
 global sounds
 sounds = False
 
-expectedVersion = "12.0.0"
+expectedVersion = "12.5.0"
 
 global maxSize
 
